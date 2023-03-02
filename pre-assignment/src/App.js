ebfb2c572b8b1fa495471f08e6498845c7976785
narin <nalynnshinhwa1128@gmail.com>
2023-03-02 19:55:14 +0900

    [FEAT] 기본 라우팅 페이지 구현

--- pre-assignment/src/App.js
+++ pre-assignment/src/App.js
@@ -1,5 +1,20 @@
+import "./index.css";
+import Home from "./routes/Home";
+import Login from "./routes/Login";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import React, { useState } from "react";
+
 function App() {
-    return;
+    const [user, setUser] = useState(null);
+    const authenticated = user != null;
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Home authenticated={authenticated} />} />
+                <Route exact path="/login" element={<Login authenticated={authenticated} />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
